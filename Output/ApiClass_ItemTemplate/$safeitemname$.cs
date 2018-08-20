3ed44f37abff64ed70f19956d16889a321bfd9f0
Adrian Gurnett <adrian.gurnett@gmail.com>
2018-08-20 13:34:16 +0100

    Add project files.

---
+++ Output/ApiClass_ItemTemplate/$safeitemname$.cs
@@ -0,0 +1,14 @@
+﻿using Serilog;
+
+namespace $rootnamespace$
+{
+    public class $safeitemname$
+    {
+        readonly ILogger _logger;
+
+        public $safeitemname$(ILogger logger)
+        {
+            _logger = logger;
+        }
+    }
+}
